f295ec62e64d414ab3d66a7c83c287fcb3d053d9
talhaimran777 <talhaimran284@gmail.com>
2023-10-20 02:10:27 +0500

    Styled form

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("Script loaded");
+
+    const popUp = document.querySelector("#pop-up");
+    const chatContainer = document.querySelector("#chat-container");
+
+    popUp.addEventListener('click', () => {
+        if (chatContainer.classList.contains("visible-pop-up")) {
+            chatContainer.classList.remove("visible-pop-up")
+            chatContainer.classList.add("hide-pop-up");
+        } else {
+            chatContainer.classList.remove("hide-pop-up")
+            chatContainer.classList.add("visible-pop-up");
+        }
+    });
 });
